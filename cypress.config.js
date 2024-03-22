d5206faa0a191390073c27226ceb926902bdc1c8
Pamela Scavuzzo <pamelascavuzzo@gmail.com>
2024-03-22 15:12:50 -0300

    new changes

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    spectPattern: "cypress/e2e/**/*.js"
+    specPattern: "cypress/e2e/**/*.js"
   },
 });
